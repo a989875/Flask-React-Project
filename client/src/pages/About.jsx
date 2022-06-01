51d5ee2e5d58b773c1d54275a53ef1dea5b42b9e
a989875 <95276834+a989875@users.noreply.github.com>
2022-06-02 01:02:25 +0800

    a little change

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from '../components/Header'
 function About(){
-
+    const style1={
+      fontSize:"36px",
+      textAlign:"center",
+    }
    return (
    <div><Header title="About"/>
+    <h1 style={style1}>This is a online shopping website!</h1>
     </div>
   )
 };
